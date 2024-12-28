70b014362c27b4d3f59fd2ad02dcc7a8280dc1f6
Yani <yani@protonmail.com>
2024-12-28 11:02:08 +0100

    add xghearth commands

---
+++ src/Commands/XGHearthCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Yani\KeeperBot\Commands;
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\Builders\MessageBuilder;
+use Yani\KeeperBot\CommandInterface;
+
+class XGHearthCommand implements CommandInterface
+{
+    public function getCommandConfig(): array
+    {
+        return [
+            'command'        => ['xgh', 'xghearth'],
+            'has_parameters' => false,
+        ];
+    }
+
+    public function handleCommand(Discord $discord, Message $message, array $parameters = []): void
+    {
+        $message->channel->sendMessage(
+            MessageBuilder::new()->addFile(__DIR__ . '/../../files/xghearth-mention.png')
+        );
+    }
+}
