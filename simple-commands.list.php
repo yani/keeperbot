c86ed6bafc9e945980957dfa725e938507615353
Yani <yani@protonmail.com>
2025-08-19 09:49:24 +0200

    rename lvl10 spell bug command

--- simple-commands.list.php
+++ simple-commands.list.php
@@ -7,7 +7,7 @@
  */
 
 return [
-    
+
     // General
     'keeperklan'      => 'https://keeperklan.com',
     'megathread'      => 'https://www.reddit.com/r/dungeonkeeper/comments/1319ns5/dungeon_keeper_megathread/',
@@ -58,7 +58,7 @@ return [
     'honhon'          => ':flag_fr: :french_bread: :croissant: hon hon',
 
     // Bugs
-    'lvl10spellbug'   => "> **Level 10 spell bug:** once a creature reaches level 10, creature spells such as Speed and Armour no longer work for its entire duration. Because of this, creatures who get these before level 10 become much weaker overall (e.g. level 9 Samurai are much more dangerous than level 10 ones). This happens because these spells are cast at the creature's experience level but spells only have 9 levels. The game looks for the level 10 value but ends up reading the next entry in the data (possibly the level 1 entry for the next spell). This affects many spells, but is particularly noticeable with Speed. Fixed in KeeperFX.",
+    'lvl10bug'        => "> **Level 10 bug:** once a creature reaches level 10, creature spells such as Speed and Armour no longer work for its entire duration. Because of this, creatures who get these before level 10 become much weaker overall (e.g. level 9 Samurai are much more dangerous than level 10 ones). This happens because these spells are cast at the creature's experience level but spells only have 9 levels. The game looks for the level 10 value but ends up reading the next entry in the data (possibly the level 1 entry for the next spell). This affects many spells, but is particularly noticeable with Speed. Fixed in KeeperFX.",
     'overflowbug'     => "> **Overflow of 8bit values:** several values in the game that would increase past 255 (or eight ones in binary) would continue counting back at 0. An example is the Dexterity value for units: when, for instance, a Horned Reaper would level up enough for his Dexterity to grow past 255, he would go from a unit that could deal out consistent damage to one that would hardly ever land a hit. This is one of the reasons level 6 Horned Reapers are more powerful than level 10 ones, the other being the abovementioned level 10 spell bug. Fixed in KeeperFX.",
 
 ];
