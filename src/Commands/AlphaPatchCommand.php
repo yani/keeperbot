b83181398f9bd4987d23a85dcb470e1df6909ece
Yani <yani@protonmail.com>
2026-02-24 15:28:04 +0100

    make alpha command use a background task + fix alpha patch fix command

--- src/Commands/AlphaPatchCommand.php
+++ src/Commands/AlphaPatchCommand.php
@@ -2,17 +2,21 @@
 
 namespace Yani\KeeperBot\Commands;
 
+use function React\Async\await;
+
 use Discord\Discord;
 use Discord\Parts\Embed\Embed;
 use Discord\Parts\Channel\Message;
 use Discord\Builders\MessageBuilder;
 
-use Yani\KeeperBot\CommandInterface;
-use Psr\Http\Message\ResponseInterface;
-
 use Yani\KeeperBot\Utility;
+use Yani\KeeperBot\BackgroundTask;
+use Yani\KeeperBot\BackgroundCommand;
 
-class AlphaPatchCommand implements CommandInterface
+use Yani\KeeperBot\CommandInterface;
+use Yani\KeeperBot\BackgroundCommandInterface;
+
+class AlphaPatchCommand extends BackgroundCommand implements CommandInterface, BackgroundCommandInterface
 {
     public function getCommandConfig(): array
     {
@@ -23,48 +27,117 @@ class AlphaPatchCommand implements CommandInterface
 
     public function handleCommand(Discord $discord, Message $message, array $parameters = []): void
     {
-        if (count($parameters) === 0) {
-            $url = $_ENV['KEEPERFX_URL'] . '/api/v1/release/alpha/latest';
-        } else {
-            $url = $_ENV['KEEPERFX_URL'] . '/api/v1/release/alpha/build/' . $parameters[0];
+        $alpha_patch = $this->getAlphaPatch($parameters);
+
+        if ($alpha_patch) {
+            $message->channel->sendEmbed($this->createEmbed($discord, $alpha_patch));
+            return;
         }
 
-        $browser = Utility::createBrowserInstance();
-        $browser
-            ->get($url)
-            ->then(function (ResponseInterface $response) use ($discord, $message) {
+        if (count($parameters) == 0) {
+            $message->channel->sendMessage(MessageBuilder::new()->setContent(
+                "Unable to find alpha patch..."
+            ));
+            return;
+        }
 
-                // Get body of response
-                $body = (string)$response->getBody();
-                if (empty($body)) {
-                    $message->reply("Failed to connect to website API...");
-                    return;
-                }
+        $build_id = (string)$parameters[0];
 
-                // Decode JSON
-                $json = \json_decode($body, true);
-                if (empty($json) || !is_array($json) || !isset($json['alpha_build']) || !is_array($json['alpha_build'])) {
-                    $message->reply("Invalid server response...");
-                    return;
-                }
+        // Tell the user the alpha build will come
+        $message->channel->sendMessage(MessageBuilder::new()->setContent(
+            "Waiting for alpha build **{$build_id}** to be ready..."
+        ));
 
-                // Handle description (add ellipsis and markdown)
-                $description = $json['alpha_build']['workflow_title'];
-                $description = \preg_replace('/\s*(\(.*?\…)/', '…', $description);
-                $description = \preg_replace('/\(\#(\d{1,6})\)/', '([#$1](https://github.com/dkfans/keeperfx/issues/$1))', $description);
+        // Add the background task
+        $this->addBackgroundTask(new BackgroundTask(
+            $discord,
+            $message,
+            [$build_id],
+            $this,
+            60,
+            30,
+            new \DateTime('now')
+        ));
+    }
 
-                // Create embed for the alpha patch
-                $embed = new Embed($discord, [
-                    'title'       => $json['alpha_build']['name'],
-                    'description' => $description,
-                    'url'         => $json['alpha_build']['download_url'],
-                    'timestamp'   => (new \DateTime($json['alpha_build']['timestamp']))->format('Y-m-d H:i'),
-                    'footer'      => ['text' => ((string) \round($json['alpha_build']['size_in_bytes'] / 1024 / 1024, 2)) . 'MiB'],
-                    'color'       => 11797236, // #b402f4
-                ]);
+    public function runBackgroundTask(Discord $discord, Message $message, array $parameters = []): bool
+    {
+        $alpha_patch = $this->getAlphaPatch($parameters);
 
-                // Send the embed as a message to the user
-                $message->channel->sendEmbed($embed);
-            });
+        if ($alpha_patch) {
+            $message->channel->sendEmbed($this->createEmbed($discord, $alpha_patch));
+            return true;
+        }
+
+        return false;
+    }
+
+    public function runBackgroundTaskTimeout(Discord $discord, Message $message, array $parameters = []): void
+    {
+        $build_id = (string)$parameters[0];
+        $message->channel->sendMessage(MessageBuilder::new()->setContent(
+            "Alpha Build [{$build_id}]: Timed out..."
+        ));
+    }
+
+    private function getApiUrlFromParameters(array $parameters = []): string
+    {
+        return $_ENV['KEEPERFX_URL'] . '/api/v1/release/alpha/' .
+            ((count($parameters) === 0) ? 'latest' : ('build/' . (string)$parameters[0]));
+    }
+
+    private function createEmbed(Discord $discord, array $alpha_patch): Embed
+    {
+        // Handle description (add ellipsis and markdown)
+        $description = $alpha_patch['workflow_title'];
+        $description = \preg_replace('/\s*(\(.*?\…)/', '…', $description);
+        $description = \preg_replace('/\(\#(\d{1,6})\)/', '([#$1](https://github.com/dkfans/keeperfx/issues/$1))', $description);
+
+        // Create embed for the alpha patch
+        return new Embed($discord, [
+            'title'       => $alpha_patch['name'],
+            'description' => $description,
+            'url'         => $alpha_patch['download_url'],
+            'timestamp'   => (new \DateTime($alpha_patch['timestamp']))->format('Y-m-d H:i'),
+            'footer'      => ['text' => ((string) \round($alpha_patch['size_in_bytes'] / 1024 / 1024, 2)) . 'MiB'],
+            'color'       => 11797236, // #b402f4
+        ]);
+    }
+
+    private function getAlphaPatch(array $parameters = []): array|false
+    {
+        try {
+
+            $browser = Utility::createBrowserInstance();
+            $promise = $browser->get($this->getApiUrlFromParameters($parameters));
+
+            $response = await($promise);
+
+            // Make sure response is not NULL
+            if (\is_null($response)) {
+                return false;
+            }
+
+            // Handle non-200 response
+            if ($response->getStatusCode() !== 200) {
+                return false;
+            }
+
+            // Get the body
+            $body = (string)$response->getBody();
+            if (empty($body)) {
+                return false;
+            }
+
+            $json = \json_decode($body, true);
+            if (empty($json) || !is_array($json) || !isset($json['alpha_build'])) {
+                return false;
+            }
+        } catch (\Exception $ex) {
+            echo $ex;
+            return false;
+        }
+
+        return $json['alpha_build'];
     }
 }
--- src/Commands/AlphaPatchFixCommand.php
+++ src/Commands/AlphaPatchFixCommand.php
@@ -6,10 +6,13 @@ use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\Builders\MessageBuilder;
 
-use Yani\KeeperBot\CommandInterface;
 use Yani\KeeperBot\Utility;
+use Yani\KeeperBot\BackgroundCommand;
 
-class AlphaPatchFixCommand implements CommandInterface
+use Yani\KeeperBot\CommandInterface;
+use Yani\KeeperBot\BackgroundCommandInterface;
+
+class AlphaPatchFixCommand extends BackgroundCommand implements CommandInterface, BackgroundCommandInterface
 {
     public function getCommandConfig(): array
     {
@@ -23,10 +26,6 @@ class AlphaPatchFixCommand implements CommandInterface
     {
         $alpha_patch_number = (int) $parameters[0];
 
-        // Handle original alpha patch command
-        $alpha_patch_command = new AlphaPatchCommand();
-        $alpha_patch_command->handleCommand($discord, $message, [$alpha_patch_number]);
-
         // Reply to the original message
         $referenced_message = $message->referenced_message;
         if ($referenced_message !== null) {
@@ -34,5 +33,21 @@ class AlphaPatchFixCommand implements CommandInterface
         } else {
             $message->reply("A fix for this can be found in Alpha {$alpha_patch_number}.");
         }
+
+        // Handle original alpha patch command
+        $alpha_patch_command = new AlphaPatchCommand();
+        $alpha_patch_command->setBackgroundTaskHandler($this->task_handler);
+        $alpha_patch_command->handleCommand($discord, $message, [$alpha_patch_number]);
+    }
+
+    public function runBackgroundTask(Discord $discord, Message $message, array $parameters = []): bool
+    {
+        // Just a placeholder because we need to pass the Background task handler to out alpha patch command
+        return false;
+    }
+
+    public function runBackgroundTaskTimeout(Discord $discord, Message $message, array $parameters = []): void
+    {
+        // Just a placeholder because we need to pass the Background task handler to out alpha patch command
     }
 }
